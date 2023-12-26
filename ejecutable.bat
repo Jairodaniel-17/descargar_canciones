7f94d633a4ed75414159a0faa3ab9821c1f0551b
Jairodaniel-17 <20b3010069@untels.edu.pe>
2023-12-26 17:19:37 -0500

    creando el entorno automaticamente

--- ejecutable.bat
+++ ejecutable.bat
@@ -1,3 +1,14 @@
+:: Crear un entorno virtual
+python -m venv .venv
+
+:: Activar el entorno virtual
 call .venv\Scripts\activate
+
+:: Instalar requisitos
+pip install -r requirements.txt
+
+:: Ejecutar la aplicación
 python app.py
+
+:: Pausa para mantener la ventana abierta
 pause
